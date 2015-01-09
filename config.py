a18ef386776cf93041a271a02c9c0075bc781c38
nlurkin <ncl@hep.ph.bham.ac.uk>
2015-01-09 13:31:08 +0000

    Add retrieval of detector reference for NIM and Prim
    Update config to get the value from the config XML file
    Update database to add the info and update it if not yet present

--- config.py
+++ config.py
@@ -12,62 +12,78 @@ class BadConfigException(Exception):
 		self.value = value
 	def __str__(self):
 		return repr(self.value)
-		
+
 class ConfigFile(object):
-    '''
-    ConfigFile
-    '''
-    
-    def __init__(self, string):
-        self.content = string
-    
-    def importFile(self):
-        """Parse XML from file"""
-        return xmld.parseString(self.content[1:-1])
-    
-    def getPropertie(self, name):
-        m = re.findall("<"+name+"\>(.*)</"+name+">", self.content)
-        if m:
-            return m[0]
-        raise BadConfigException(name);
-    
-    def getValue(self, nodelist):
-        """Return the text contained in a node ( <node>this text</node> )"""
-        rc = []
-        for node in nodelist:
-            if node.nodeType == node.TEXT_NODE:
-                rc.append(node.data)
-        return ''.join(rc)
-    
-    
-    def findNIMMask(self, address):
-        try:
-            doc = self.importFile()
-        except:
-            raise BadConfigException("Unable to parse for NIM " + str(address))
-        rootNode = doc.getElementsByTagName("LUT_parameters_NIM")[0]
-        itemNodes = rootNode.getElementsByTagName("item")[address]
-        globalNode = doc.getElementsByTagName("global_parameters")[0]
-        downscalNode = globalNode.getElementsByTagName("downScal_NIM_mask")[0]
-        downscalList = downscalNode.getElementsByTagName("item")[address]
-        row = []
-        row.append(self.getValue(itemNodes.getElementsByTagName("lut_detAmask")[0].childNodes))
-        row.append(self.getValue(itemNodes.getElementsByTagName("lut_detBmask")[0].childNodes))
-        row.append(self.getValue(itemNodes.getElementsByTagName("lut_detCmask")[0].childNodes))
-        row.append(self.getValue(itemNodes.getElementsByTagName("lut_detDmask")[0].childNodes))
-        row.append(self.getValue(itemNodes.getElementsByTagName("lut_detEmask")[0].childNodes))
-        return ''.join(row) + ":" + str(self.getValue(downscalList.childNodes))
-    
-    def findPrimMask(self, address):
-        try:
-            doc = self.importFile()
-        except:
-            raise BadConfigException("Unable to parse for Prim " + str(address)) 
-        rootNode = doc.getElementsByTagName("LUT_parameters")[0]
-        itemNodes = rootNode.getElementsByTagName("item")[address]
-        row = []
-        row.append(self.getValue(itemNodes.getElementsByTagName("lut_detAmask")[0].childNodes).zfill(2))
-        row.append(self.getValue(itemNodes.getElementsByTagName("lut_detBmask")[0].childNodes).zfill(2))
-        row.append(self.getValue(itemNodes.getElementsByTagName("lut_detCmask")[0].childNodes).zfill(2))
-        return ''.join(row)
-        
+	'''
+	ConfigFile
+	'''
+	
+	def __init__(self, string):
+		self.content = string
+	
+	def importFile(self):
+		"""Parse XML from file"""
+		return xmld.parseString(self.content[1:-1])
+	
+	def getPropertie(self, name):
+		m = re.findall("<"+name+"\>(.*)</"+name+">", self.content)
+		if m:
+			return m[0]
+		raise BadConfigException(name);
+	
+	def getValue(self, nodelist):
+		"""Return the text contained in a node ( <node>this text</node> )"""
+		rc = []
+		for node in nodelist:
+			if node.nodeType == node.TEXT_NODE:
+				rc.append(node.data)
+		return ''.join(rc)
+	
+	
+	def findNIMMask(self, address):
+		try:
+			doc = self.importFile()
+		except:
+			raise BadConfigException("Unable to parse for NIM " + str(address))
+		rootNode = doc.getElementsByTagName("LUT_parameters_NIM")[0]
+		itemNodes = rootNode.getElementsByTagName("item")[address]
+		globalNode = doc.getElementsByTagName("global_parameters")[0]
+		downscalNode = globalNode.getElementsByTagName("downScal_NIM_mask")[0]
+		downscalList = downscalNode.getElementsByTagName("item")[address]
+		row = []
+		row.append(self.getValue(itemNodes.getElementsByTagName("lut_detAmask")[0].childNodes))
+		row.append(self.getValue(itemNodes.getElementsByTagName("lut_detBmask")[0].childNodes))
+		row.append(self.getValue(itemNodes.getElementsByTagName("lut_detCmask")[0].childNodes))
+		row.append(self.getValue(itemNodes.getElementsByTagName("lut_detDmask")[0].childNodes))
+		row.append(self.getValue(itemNodes.getElementsByTagName("lut_detEmask")[0].childNodes))
+		return ''.join(row) + ":" + str(self.getValue(downscalList.childNodes))
+	
+	def findPrimMask(self, address):
+		try:
+			doc = self.importFile()
+		except:
+			raise BadConfigException("Unable to parse for Prim " + str(address)) 
+		rootNode = doc.getElementsByTagName("LUT_parameters")[0]
+		itemNodes = rootNode.getElementsByTagName("item")[address]
+		row = []
+		row.append(self.getValue(itemNodes.getElementsByTagName("lut_detAmask")[0].childNodes).zfill(2))
+		row.append(self.getValue(itemNodes.getElementsByTagName("lut_detBmask")[0].childNodes).zfill(2))
+		row.append(self.getValue(itemNodes.getElementsByTagName("lut_detCmask")[0].childNodes).zfill(2))
+		return ''.join(row)
+		
+
+	def getRefDetNim(self):
+		try:
+			doc = self.importFile()
+		except:
+			raise BadConfigException("Unable to parse for referenceDet_NIM")
+		referenceDet = self.getValue(doc.getElementsByTagName("referenceDet_NIM")[0].childNodes)
+		return int(referenceDet)
+	
+	def getRefDetPrim(self):
+		try:
+			doc = self.importFile()
+		except:
+			raise BadConfigException("Unable to parse for referenceDet")
+		referenceDet = self.getValue(doc.getElementsByTagName("referenceDet")[0].childNodes)
+		return int(referenceDet)
